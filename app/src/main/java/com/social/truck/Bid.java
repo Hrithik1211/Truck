28d3c89341bc20f420b156886d16ac149c30192e
Hrithik1211 <hs.hp121@gmail.com>
2020-07-19 20:10:05 +0530

    tracking

---
+++ app/src/main/java/com/social/truck/Bid.java
@@ -0,0 +1,58 @@
+package com.social.truck;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+
+public class Animation extends Fragment {
+
+
+
+
+
+
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.animation,container,false);
+
+    }
+
+
+    String uid ;
+
+    @Override
+    public void onViewCreated(@NonNull final View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        ImageView imageView = (ImageView) view.findViewById(R.id.imagetick);
+        imageView.setAlpha(0f);
+        imageView.animate().alpha(1f).setDuration(1000);
+    }
+
+
+}
